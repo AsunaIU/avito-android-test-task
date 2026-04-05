ed4a82479337734e40acbc6ab7a01f8f8bf0f45e
AsunaIU <valnevann@gmail.com>
2026-04-05 22:11:30 +0500

    setup room database

---
+++ app/src/main/java/io/valneva/chatassistant/core/data/local/AppDatabase.kt
@@ -0,0 +1,23 @@
+package io.valneva.chatassistant.core.data.local
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import io.valneva.chatassistant.core.data.local.dao.ChatDao
+import io.valneva.chatassistant.core.data.local.dao.MessageDao
+import io.valneva.chatassistant.core.data.local.entity.ChatEntity
+import io.valneva.chatassistant.core.data.local.entity.MessageEntity
+
+@Database(
+    entities = [
+        ChatEntity::class,
+        MessageEntity::class,
+    ],
+    version = 1,
+    exportSchema = false,
+)
+@TypeConverters(RoomConverters::class)
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun chatDao(): ChatDao
+    abstract fun messageDao(): MessageDao
+}
